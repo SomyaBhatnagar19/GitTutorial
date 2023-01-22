1865733a2bb3658ed16f3f43e01ede4bf829b817
SomyaBhatnagar19 <122341588+SomyaBhatnagar19@users.noreply.github.com>
2023-01-22 11:01:26 +0530

    getElementsByClassName usage

--- demo.js
+++ demo.js
@@ -1,1 +1,8 @@
-console.log('Hello world');
+//GET ELEMENTS BY CLASS NAME//
+let items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor = 'green';
+for(let i=0; i<items.length; i++) {
+    items[i].style.fontWeight = 'bold';
+}
+

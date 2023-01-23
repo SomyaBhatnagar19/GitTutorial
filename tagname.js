dd4e64aa5c1f081a7561e18eb260218a072e25e5
SomyaBhatnagar19 <122341588+SomyaBhatnagar19@users.noreply.github.com>
2023-01-23 11:31:17 +0530

    tagname trial

---
+++ tagname.js
@@ -0,0 +1,17 @@
+//GET ELEMENTS BY CLASS NAME//
+//let li = document.getElementsByTagName('li');
+//console.log(li);
+//li[2].style.backgroundColor = 'green';
+//for(let i=0; i<li.length; i++) {
+//    li[i].style.fontWeight = 'bold';
+//}
+
+//Modifying new item 5 with classname//
+//let five = document.getElementsByClassName('fv');
+//console.log(five);
+//five[0].style.backgroundColor = 'purple';
+
+//Modifying new item 5 with tagname//
+let five = document.getElementsByTagName('li');
+console.log(five);
+five[4].style.backgroundColor = 'green';
